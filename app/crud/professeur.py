e263694e75b8ec4b8b73694247c2e8c2bb397451
Robin HOTTON <robin.hotton@hotmail.com>
2024-07-22 17:51:48 +0200

    get by id

--- app/crud/professeur.py
+++ app/crud/professeur.py
@@ -4,12 +4,10 @@ from bson import ObjectId
 
 collection: Collection = db["professeurs"]
 
-def convert_object_ids(document):
-    """Convert ObjectId to str in a MongoDB document."""
-    if "_id" in document:
-        document["_id"] = str(document["_id"])
-    return document
-
 def get_all_professeurs():
-    professeurs = list(collection.find())
-    return [convert_object_ids(professeur) for professeur in professeurs]
+    professeurs = collection.find({}, {"_id": 0})
+    return list(professeurs)
+
+def get_professeur(professeur_id: str):
+    professeur = collection.find_one({"_id": ObjectId(professeur_id)}, {"_id": 0})
+    return professeur
--- app/routers/professeur.py
+++ app/routers/professeur.py
@@ -6,3 +6,7 @@ router = APIRouter(tags=["Professeurs"], prefix="/professeurs")
 @router.get("/")
 def get_all_professeurs():
     return crud.get_all_professeurs()
+
+@router.get("/{professeur_id}")
+def get_professeur(professeur_id: str):
+    return crud.get_professeur(professeur_id)
