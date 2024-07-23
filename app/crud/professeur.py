d934d8d37cead48f9a6cf7ec27fa7a614b8118cb
Robin HOTTON <robin.hotton@hotmail.com>
2024-07-23 09:26:49 +0200

    crud professeur

--- app/crud/professeur.py
+++ app/crud/professeur.py
@@ -5,9 +5,19 @@ from bson import ObjectId
 collection: Collection = db["professeurs"]
 
 def get_all_professeurs():
-    professeurs = collection.find({}, {"_id": 0})
-    return list(professeurs)
+    return list(collection.find({}, {"_id": 0}))
 
 def get_professeur(professeur_id: str):
-    professeur = collection.find_one({"_id": ObjectId(professeur_id)}, {"_id": 0})
-    return professeur
+    return collection.find_one({"_id": ObjectId(professeur_id)}, {"_id": 0})
+
+def create_professeur(professeur: dict):
+    result = collection.insert_one(professeur)
+    return {"_id": str(result.inserted_id)}
+
+def update_professeur(professeur_id: str, professeur: dict):
+    result = collection.update_one({"_id": ObjectId(professeur_id)}, {"$set": professeur})
+    return {"matched_count": result.matched_count, "modified_count": result.modified_count}
+
+def delete_professeur(professeur_id: str):
+    result = collection.delete_one({"_id": ObjectId(professeur_id)})
+    return {"deleted_count": result.deleted_count}
--- app/routers/professeur.py
+++ app/routers/professeur.py
@@ -10,3 +10,15 @@ def get_all_professeurs():
 @router.get("/{professeur_id}")
 def get_professeur(professeur_id: str):
     return crud.get_professeur(professeur_id)
+
+@router.post("/")
+def create_professeur(professeur: dict):
+    return crud.create_professeur(professeur)
+
+@router.put("/{professeur_id}")
+def update_professeur(professeur_id: str, professeur: dict):
+    return crud.update_professeur(professeur_id, professeur)
+
+@router.delete("/{professeur_id}")
+def delete_professeur(professeur_id: str):
+    return crud.delete_professeur(professeur_id)
